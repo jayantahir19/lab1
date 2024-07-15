f1497653b05283263fb2d2d579207bc7b4c6582a
jayantahir19 <bca2022jayant1788@tnraocollege.org>
2024-07-15 18:38:52 +0530

    lab1

---
+++ pro4.py
@@ -0,0 +1,13 @@
+EMPNAME = []
+
+EMPNAME. append("jayant")
+EMPNAME. append("umesh")
+EMPNAME.append("mahesh")
+
+print("Employee Names after additions:", EMPNAME)
+EMPNAME.remove("jayant")
+
+print("Employee Names after removal  :", EMPNAME)
+EMPNAME. append("umesh")
+
+print("Final Employee Names          :", EMPNAME)
